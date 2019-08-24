cecfbb74007e9d07a3d49655385af1b52994fe41
Leandro Hanc <leandrohanc@gmail.com>
2019-08-24 17:51:02 -0300

    Redactar respuesta en About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,9 +4,24 @@ import './About.sass';
 
 const About = () => (
   <section className="About">
-    <div> ¿QUÉ ES ESTO?</div>
-    &nbsp;-&nbsp;
-    <div> ¿QUIÉN LO HIZO?</div>
+
+    <div> ¿QUÉ ES ESTO?
+    Es un programa que intenta simular a Alberto Fernández.
+    Pero no sólo al Alberto en su faceta como
+    candidato en campaña electoral, sino a la persona.
+    La mayoría de las respuestas son tomas de entrevistas o
+    tweets.
+    </div>
+
+    <div> ¿QUIÉN LO HIZO?
+    Un pequeño grupo de compañeres en apoyo a la fórmula
+    Fernández-Fernández.
+    El proyecto inició con la idea de recoger las simpáticas
+    respuestas de Alberto en Twitter y luego fue mutando en
+    algo más serio y complejo: que responda también cualquier tema
+    relevante como candidato a presidente.
+    </div>
+
   </section>
 );
 
